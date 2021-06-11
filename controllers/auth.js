da2691d32d2114256e61bfc323d916a7f39320cc
Your Name <lamisazamzam@gmail.com>
2021-06-11 19:46:14 +0600

    initally done

--- controllers/auth.js
+++ controllers/auth.js
@@ -45,8 +45,31 @@ exports.login = async (req, res, next) => {
         });
     }
 };
-exports.forgotPassword = (req, res, next) => {
-    res.send("Forgot Password");
+exports.forgotPassword = async (req, res, next) => {
+    const { email } = req.body;
+
+    try {
+        const user = await User.findOne({ email });
+
+        if (!user) {
+            return next(new ErrorResponse("Email could not be sent.", 404));
+        }
+
+        const resetToken = user.getResetPasswordToken();
+
+        await user.save();
+
+        const resetUrl = `http://localhost:3000/passwordReset/${resetToken}`;
+
+        const message = `
+        <h1>You have requested a password reset</h1>
+        <p>Please go to this link to reset your password</p>
+        <a href="${resetUrl}" clicktracking="off">${resetUrl}</a>
+        `;
+
+        try {
+        } catch (err) {}
+    } catch (err) {}
 };
 exports.resetPassword = (req, res, next) => {
     res.send("Reset Password");
--- index.js
+++ index.js
@@ -12,6 +12,8 @@ app.use(express.json());
 
 app.use("/api/auth", require("./routes/auth"));
 
+app.use("/api/private", require("./routes/private"));
+
 app.get("/", (req, res) => {
     res.send("Hello World!");
 });
