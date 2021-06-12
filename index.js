cbdcc2c11ae9c374ee293234c81ab87180d4dade
Your Name <lamisazamzam@gmail.com>
2021-06-12 19:08:00 +0600

    added config vars

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ app.get("/", (req, res) => {
 
 app.use(errorHandler);
 
-const server = app.listen(5000, () => {
+const server = app.listen(process.env.PORT || 5000, () => {
     console.log(`Example app listening`);
 });
 
